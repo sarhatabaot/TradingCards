a4b9da3d434ffe5808766cab481c8266c891f47e
Omer Oreg <sarhatabaot1@gmail.com>
2021-06-22 21:43:16 +0300

    Update CardUtil.java

--- src/main/java/media/xen/tradingcards/CardUtil.java
+++ src/main/java/media/xen/tradingcards/CardUtil.java
@@ -19,7 +19,6 @@ import org.jetbrains.annotations.Nullable;
 
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
