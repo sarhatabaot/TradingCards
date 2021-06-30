e7366846e4b1542c346e7537fbfd65236690b845
Omer Oreg <sarhatabaot1@gmail.com>
2021-06-30 11:17:02 +0300

    Created PackManager

---
+++ tradingcards-api/src/main/java/net/tinetwork/tradingcards/tradingcardsapi/manager/PackManager.java
@@ -0,0 +1,16 @@
+package net.tinetwork.tradingcards.tradingcardsapi.manager;
+
+import org.bukkit.inventory.ItemStack;
+
+import java.util.List;
+import java.util.Map;
+
+public abstract class PackManager {
+    public abstract Map<String,ItemStack> packs();
+
+    public ItemStack getPackItem(String name) {
+        return packs().get(name);
+    }
+
+    public abstract ItemStack generatePack(String name);
+}
