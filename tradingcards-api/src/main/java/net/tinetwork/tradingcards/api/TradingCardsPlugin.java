43bd04e674a18fd70167b566ddbd66d260bd4c72
Omer Oreg <sarhatabaot1@gmail.com>
2021-07-15 23:21:23 +0300

    More work starting on docs

--- tradingcards-api/src/main/java/net/tinetwork/tradingcards/api/TradingCardsPlugin.java
+++ tradingcards-api/src/main/java/net/tinetwork/tradingcards/api/TradingCardsPlugin.java
@@ -10,22 +10,62 @@ import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public abstract class TradingCardsPlugin<T> extends JavaPlugin {
-
+    /**
+     *
+     * @param message
+     */
     public abstract void debug(String message);
 
+    /**
+     *
+     * @param string
+     * @return
+     */
     public abstract boolean isMob(String string);
 
+
+    /**
+     *
+     * @param type
+     * @return
+     */
     public abstract boolean isMob(EntityType type);
 
+    /**
+     *
+     * @return
+     */
     public abstract CardManager<T> getCardManager();
 
+    /**
+     *
+     * @return
+     */
     public abstract PackManager getPackManager();
 
+    /**
+     *
+     * @return
+     */
     public abstract DeckManager getDeckManager();
 
+    /**
+     *
+     * @return
+     */
     public abstract Blacklist<Player> getPlayerBlacklist();
 
+
+    /**
+     *
+     * @return
+     */
     public abstract Blacklist<World> getWorldBlacklist();
 
+
+    /**
+     *
+     * @return
+     */
     public abstract TradingCardsPlugin<T> get();
 }
