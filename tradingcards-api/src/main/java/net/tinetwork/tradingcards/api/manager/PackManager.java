2fb994f518c2c9c558df95ed83b52d5fc3cb62ef
Omer Oreg <sarhatabaot1@gmail.com>
2021-08-21 11:25:08 +0300

    Added a getPack method to PackManager

--- tradingcards-api/src/main/java/net/tinetwork/tradingcards/api/manager/PackManager.java
+++ tradingcards-api/src/main/java/net/tinetwork/tradingcards/api/manager/PackManager.java
@@ -1,5 +1,6 @@
 package net.tinetwork.tradingcards.api.manager;
 
+import net.tinetwork.tradingcards.api.model.Pack;
 import org.bukkit.inventory.ItemStack;
 
 import java.util.Map;
@@ -14,5 +15,7 @@ public abstract class PackManager {
     public abstract ItemStack generatePack(String name);
 
     public abstract boolean isPack(final ItemStack item);
+
+    public abstract Pack getPack(final String id);
 
 }
