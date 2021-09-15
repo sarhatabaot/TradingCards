76ffe1fc5cfedcb2b4a9efe02770922b9cdd1660
Omer Oreg <sarhatabaot1@gmail.com>
2021-09-15 21:22:27 +0300

    Update CardsConfig.java

--- tradingcards-plugin/src/main/java/net/tinetwork/tradingcards/tradingcardsplugin/config/CardsConfig.java
+++ tradingcards-plugin/src/main/java/net/tinetwork/tradingcards/tradingcardsplugin/config/CardsConfig.java
@@ -7,41 +7,58 @@ import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 
-public class CardsConfig{
-	private final List<SimpleCardsConfig> cardConfigs;
+public class CardsConfig {
+    private final File cardsFolder;
+    private final TradingCards plugin;
 
-	public CardsConfig(final TradingCards plugin) {
-		this.cardConfigs = new ArrayList<>();
-		createCardsFolder(plugin);
-		createDefaultCardConfig(plugin);
-		File cardsFolder = new File(plugin.getDataFolder().getPath()+File.separator+"cards");
+    private List<SimpleCardsConfig> cardConfigs;
+    public CardsConfig(final TradingCards plugin) {
+        this.plugin = plugin;
 
-		for(File file: cardsFolder.listFiles()) {
-			plugin.debug(file.getName());
-			if(file.getName().endsWith(".yml")) {
-				plugin.debug("Added: "+file.getName());
-				try {
-					cardConfigs.add(new SimpleCardsConfig(plugin, file.getName()));
-				}catch (ConfigurateException e) {
-					plugin.getLogger().severe(e.getMessage());
-				}
-			}
-		}
-	}
-	private static void createCardsFolder(final TradingCards plugin) {
-		final File cardsFolder = new File(plugin.getDataFolder()+File.separator+"cards");
-		if(!cardsFolder.exists())
-			cardsFolder.mkdir();
-	}
+        createCardsFolder(plugin);
+        if(plugin.getGeneralConfig().useDefaultCardsFile())
+            createDefaultCardConfig(plugin);
 
-	private static void createDefaultCardConfig(final TradingCards plugin) {
-		try {
-			new SimpleCardsConfig(plugin, "cards.yml").saveDefaultConfig();
-		} catch (ConfigurateException e) {
-			plugin.getLogger().severe(e.getMessage());
-		}
-	}
-	public List<SimpleCardsConfig> getCardConfigs() {
-		return cardConfigs;
-	}
+        this.cardsFolder = new File(plugin.getDataFolder().getPath() + File.separator + "cards");
+
+        initValues();
+    }
+
+    public void initValues() {
+        this.cardConfigs = new ArrayList<>();
+        if(cardsFolder.listFiles() == null) {
+            plugin.getLogger().warning("There are no files in the cards folder.");
+            return;
+        }
+
+        for (File file : cardsFolder.listFiles()) {
+            plugin.debug("File name: " + file.getName());
+            if (file.getName().endsWith(".yml")) {
+                try {
+                    cardConfigs.add(new SimpleCardsConfig(plugin, file.getName()));
+                    plugin.debug("Added: " + file.getName());
+                } catch (ConfigurateException e) {
+                    plugin.getLogger().severe(e.getMessage());
+                }
+            }
+        }
+    }
+
+    private void createCardsFolder(final TradingCards plugin) {
+        final File cardsFolder = new File(plugin.getDataFolder() + File.separator + "cards");
+        if (!cardsFolder.exists())
+            cardsFolder.mkdir();
+    }
+
+    private void createDefaultCardConfig(final TradingCards plugin) {
+        try {
+            new SimpleCardsConfig(plugin, "cards.yml").saveDefaultConfig();
+        } catch (ConfigurateException e) {
+            plugin.getLogger().severe(e.getMessage());
+        }
+    }
+
+    public List<SimpleCardsConfig> getCardConfigs() {
+        return cardConfigs;
+    }
 }
