960da4f6424bb2a18ef1f6e3f31774500601eaa3
Omer Oreg <sarhatabaot1@gmail.com>
2021-09-29 19:27:47 +0300

    Update TradingCardsPlugin.java

--- tradingcards-api/src/main/java/net/tinetwork/tradingcards/api/TradingCardsPlugin.java
+++ tradingcards-api/src/main/java/net/tinetwork/tradingcards/api/TradingCardsPlugin.java
@@ -7,9 +7,22 @@ import net.tinetwork.tradingcards.api.manager.PackManager;
 import org.bukkit.World;
 import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
+import org.bukkit.plugin.PluginDescriptionFile;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.plugin.java.JavaPluginLoader;
+import org.jetbrains.annotations.NotNull;
+
+import java.io.File;
 
 public abstract class TradingCardsPlugin<T> extends JavaPlugin {
+    public TradingCardsPlugin() {
+        super();
+    }
+
+    public TradingCardsPlugin(final @NotNull JavaPluginLoader loader, final @NotNull PluginDescriptionFile description, final @NotNull File dataFolder, final @NotNull File file) {
+        super(loader, description, dataFolder, file);
+    }
+
     /**
      * Send a debug message in the log.
      * @param message Message to send.
