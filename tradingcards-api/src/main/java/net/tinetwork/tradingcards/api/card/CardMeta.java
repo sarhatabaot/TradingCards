0431f1a05df4a7e882fb10598cd32c3bcf78433d
Omer Oreg <sarhatabaot1@gmail.com>
2021-10-03 11:50:37 +0300

    Added CardMeta

---
+++ tradingcards-api/src/main/java/net/tinetwork/tradingcards/api/card/CardMeta.java
@@ -0,0 +1,12 @@
+package net.tinetwork.tradingcards.api.card;
+
+/**
+ * @author sarhatabaot
+ */
+public class CardMeta {
+    private int customModelNbt;
+    private double buyPrice;
+    private double sellPrice;
+    private String displayName;
+    private boolean shiny;
+}
