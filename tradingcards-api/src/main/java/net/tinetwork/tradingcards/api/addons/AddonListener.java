83405f5b8b630330eaa51b683a2eaaa55a466893
Omer Oreg <sarhatabaot1@gmail.com>
2021-10-12 09:35:25 +0300

    Update AddonListener.java

--- tradingcards-api/src/main/java/net/tinetwork/tradingcards/api/addons/AddonListener.java
+++ tradingcards-api/src/main/java/net/tinetwork/tradingcards/api/addons/AddonListener.java
@@ -3,10 +3,10 @@ package net.tinetwork.tradingcards.api.addons;
 import org.bukkit.event.Listener;
 
 public class AddonListener implements Listener {
-	protected TradingCardsAddon tradingCardsAddon;
+	protected TradingCardsAddon addon;
 
 	public AddonListener(final TradingCardsAddon tradingCardsAddon) {
-		this.tradingCardsAddon = tradingCardsAddon;
+		this.addon = tradingCardsAddon;
 	}
 
 }
