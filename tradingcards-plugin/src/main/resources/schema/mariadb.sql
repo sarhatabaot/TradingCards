a21d59f2cf1b0a5d276c7c757442d71df31bc14d
Omer Oreg <sarhatabaot1@gmail.com>
2022-02-04 11:39:52 +0200

    Increased card & rarity id char limit

--- tradingcards-plugin/src/main/resources/schema/mariadb.sql
+++ tradingcards-plugin/src/main/resources/schema/mariadb.sql
@@ -1,13 +1,11 @@
 -- TradingCards MariaSql Schema
-
 CREATE TABLE `{prefix}decks` (
     `id`            INT AUTO_INCREMENT NOT NULL,
     `uuid`          VARCHAR(36)        NOT NULL,
     `deck_number`   INT                NOT NULL,
-    `card_id`       VARCHAR(36)        NOT NULL,
-    `rarity_id`     VARCHAR(36)        NOT NULL,
+    `card_id`       VARCHAR(200)        NOT NULL,
+    `rarity_id`     VARCHAR(200)        NOT NULL,
     `amount`        INT                NOT NULL,
     `is_shiny`      BOOL               NOT NULL,
-    `slot`          INT                NOT NULL,
     PRIMARY KEY (`id`)
 ) DEFAULT CHARSET = utf8mb4;
