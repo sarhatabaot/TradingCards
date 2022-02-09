3e51069ab10eef0ecd3a19be721989391afc94ac
Omer Oreg <sarhatabaot1@gmail.com>
2022-02-09 18:44:27 +0200

    Fixes #76 #75

--- tradingcards-api/src/main/java/net/tinetwork/tradingcards/api/model/deck/StorageEntry.java
+++ tradingcards-api/src/main/java/net/tinetwork/tradingcards/api/model/deck/StorageEntry.java
@@ -2,7 +2,11 @@ package net.tinetwork.tradingcards.api.model.deck;
 
 import java.util.Objects;
 
-public record StorageEntry(String rarityId, String cardId, int amount, boolean isShiny) {
+public class StorageEntry {
+    private final String rarityId;
+    private final String cardId;
+    private int amount;
+    private final boolean isShiny;
 
     public String getRarityId() {
         return rarityId;
@@ -14,6 +18,21 @@ public record StorageEntry(String rarityId, String cardId, int amount, boolean i
 
     public int getAmount() {
         return amount;
+    }
+
+    public void setAmount(int amount) {
+        this.amount = amount;
+    }
+
+    public boolean isShiny() {
+        return isShiny;
+    }
+
+    public StorageEntry(final String rarityId, final String cardId, final int amount, final boolean isShiny) {
+        this.rarityId = rarityId;
+        this.cardId = cardId;
+        this.amount = amount;
+        this.isShiny = isShiny;
     }
 
     @Override
@@ -44,6 +63,13 @@ public record StorageEntry(String rarityId, String cardId, int amount, boolean i
         if (o == null || getClass() != o.getClass()) return false;
         final StorageEntry that = (StorageEntry) o;
         return amount == that.amount && isShiny == that.isShiny && Objects.equals(rarityId, that.rarityId) && Objects.equals(cardId, that.cardId);
+    }
+
+    public boolean isSimilar(final Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        final StorageEntry that = (StorageEntry) o;
+        return isShiny == that.isShiny && Objects.equals(rarityId, that.rarityId) && Objects.equals(cardId, that.cardId);
     }
 
     @Override
