bcba4395f19f29eae4120f7687019b17f4c5d00a
Omer Oreg <sarhatabaot1@gmail.com>
2022-02-09 18:49:47 +0200

    Now checking if the table exists

--- tradingcards-plugin/src/main/resources/schema/mysql.sql
+++ tradingcards-plugin/src/main/resources/schema/mysql.sql
@@ -11,7 +11,7 @@ CREATE TABLE IF NOT EXISTS `{prefix}decks` (
     PRIMARY KEY (id)
 ) DEFAULT CHARSET = utf8mb4;
 
-CREATE TABLE `{prefix}rarities` (
+CREATE TABLE IF NOT EXISTS `{prefix}rarities` (
     rarity_id      VARCHAR(200)       NOT NULL, --Foreign key for rewards
     display_name   TINYTEXT           NOT NULL,
     default_color  VARCHAR(36)        NOT NULL,
@@ -20,7 +20,7 @@ CREATE TABLE `{prefix}rarities` (
     PRIMARY KEY (rarity_id)
 ) DEFAULT CHARSET = utf8mb4;
 
-CREATE TABLE `{prefix}rewards` (
+CREATE TABLE IF NOT EXISTS `{prefix}rewards` (
     rarity_id     VARCHAR(200)       NOT NULL, --This should just be the rarity_id, we don't even need a dedicated rewards_id. (We need one in the table but not in the rarities table)
     command       TINYTEXT           NOT NULL,
     order_number  INT                NOT NULL,
@@ -28,14 +28,14 @@ CREATE TABLE `{prefix}rewards` (
     FOREIGN KEY (rarity_id) REFERENCES `{prefix}rarities`(rarity_id)
 ) DEFAULT CHARSET = utf8mb4;
 
-CREATE TABLE `{prefix}series` (
+CREATE TABLE IF NOT EXISTS `{prefix}series` (
     series_id    VARCHAR(200)                           NOT NULL, --Foreign key for series colors (We should define as a foreign key)
     display_name TINYTEXT                               NOT NULL,
     series_mode  ENUM('ACTIVE','DISABLED','SCHEDULED')  NOT NULL,
     PRIMARY KEY (series_id)
 ) DEFAULT CHARSET = utf8mb4;
 
-CREATE TABLE `{prefix}series_colors` (
+CREATE TABLE IF NOT EXISTS `{prefix}series_colors` (
    series_id    VARCHAR(200)       NOT NULL,
    color        VARCHAR(10)        NOT NULL,
    `type`       VARCHAR(10)        NOT NULL,
@@ -46,14 +46,14 @@ CREATE TABLE `{prefix}series_colors` (
    FOREIGN KEY (series_id) REFERENCES `{prefix}series`(series_id)
 ) DEFAULT CHARSET = utf8mb4;
 
-CREATE TABLE `{prefix}custom_types` (
+CREATE TABLE IF NOT EXISTS `{prefix}custom_types` (
     type_id         VARCHAR(200)       NOT NULL,
     display_name    TINYTEXT           NOT NULL,
     drop_type       ENUM('boss','hostile','neutral','passive','all')
     PRIMARY KEY (type_id)
 ) DEFAULT CHARSET = utf8mb4;
 
-CREATE TABLE `{prefix}packs` (
+CREATE TABLE IF NOT EXISTS `{prefix}packs` (
     pack_id         VARCHAR(200)       NOT NULL, -- Foreign key for packs_content
     display_name    TINYTEXT           NOT NULL,
     price           DOUBLE             NOT NULL,
@@ -61,7 +61,7 @@ CREATE TABLE `{prefix}packs` (
     PRIMARY KEY (pack_id)
 ) DEFAULT CHARSET = utf8mb4;
 
-CREATE TABLE `{prefix}packs_content` (
+CREATE TABLE IF NOT EXISTS `{prefix}packs_content` (
     order_number     INT                NOT NULL,
     pack_id          VARCHAR(200)       NOT NULL,
     rarity_id        VARCHAR(200)       NOT NULL, -- foreign key from rarities
@@ -72,7 +72,7 @@ CREATE TABLE `{prefix}packs_content` (
     FOREIGN KEY (series_id) REFERENCES `{prefix}series`(series_id)
 ) DEFAULT CHARSET = utf8mb4;
 
-CREATE TABLE `{prefix}cards` (
+CREATE TABLE IF NOT EXISTS `{prefix}cards` (
     card_id             VARCHAR(200)        NOT NULL,
     display_name        TINYTEXT,
     rarity_id           VARCHAR(200)        NOT NULL, -- foreign key from rarities
