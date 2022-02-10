0bcdaf12a4af10da38ac0169fd6125485aaca10a
Omer Oreg <sarhatabaot1@gmail.com>
2022-02-10 11:38:30 +0200

    Update RarityManager.java

--- tradingcards-api/src/main/java/net/tinetwork/tradingcards/api/manager/RarityManager.java
+++ tradingcards-api/src/main/java/net/tinetwork/tradingcards/api/manager/RarityManager.java
@@ -2,9 +2,12 @@ package net.tinetwork.tradingcards.api.manager;
 
 import net.tinetwork.tradingcards.api.model.Rarity;
 
+import java.util.List;
+
 /**
  * @author sarhatabaot
  */
 public interface RarityManager {
     Rarity getRarity(final String rarityId);
+    List<Rarity> getRarities();
 }
