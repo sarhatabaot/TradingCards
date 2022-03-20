498ef6111280a5db357446cd6a26bef47b4b74ee
Omer Oreg <sarhatabaot1@gmail.com>
2022-03-20 12:56:46 +0200

    Update mysql.sql

--- tradingcards-plugin/src/main/resources/schema/mysql.sql
+++ tradingcards-plugin/src/main/resources/schema/mysql.sql
@@ -8,13 +8,11 @@ CREATE TABLE IF NOT EXISTS `{prefix}decks` (
     rarity_id      VARCHAR(200)       NOT NULL,
     amount         INT                NOT NULL,
     is_shiny       BOOL               NOT NULL,
-    PRIMARY KEY (id),
-    FOREIGN KEY (rarity_id) REFERENCES `{prefix}rarities`(rarity_id),
-    FOREIGN KEY (card_id) REFERENCES `{prefix}cards`(card_id)
+    PRIMARY KEY (id)
 ) DEFAULT CHARSET = utf8mb4;
 
 CREATE TABLE IF NOT EXISTS `{prefix}rarities` (
-    rarity_id      VARCHAR(200)       NOT NULL, --Foreign key for rewards
+    rarity_id      VARCHAR(200)       NOT NULL,
     display_name   TINYTEXT           NOT NULL,
     default_color  VARCHAR(36)        NOT NULL,
     buy_price      DOUBLE             NOT NULL,
@@ -23,15 +21,14 @@ CREATE TABLE IF NOT EXISTS `{prefix}rarities` (
 ) DEFAULT CHARSET = utf8mb4;
 
 CREATE TABLE IF NOT EXISTS `{prefix}rewards` (
-    rarity_id      VARCHAR(200)       NOT NULL, --This should just be the rarity_id, we don't even need a dedicated rewards_id. (We need one in the table but not in the rarities table)
+    rarity_id      VARCHAR(200)       NOT NULL,
     command        TINYTEXT           NOT NULL,
     command_order  INT               NOT NULL,
     PRIMARY KEY (rarity_id)
-    FOREIGN KEY (rarity_id) REFERENCES `{prefix}rarities`(rarity_id)
 ) DEFAULT CHARSET = utf8mb4;
 
 CREATE TABLE IF NOT EXISTS `{prefix}series` (
-    series_id    VARCHAR(200)                           NOT NULL, --Foreign key for series colors (We should define as a foreign key)
+    series_id    VARCHAR(200)                           NOT NULL,
     display_name TINYTEXT                               NOT NULL,
     series_mode  ENUM('ACTIVE','DISABLED','SCHEDULED')  NOT NULL,
     PRIMARY KEY (series_id)
@@ -45,18 +42,17 @@ CREATE TABLE IF NOT EXISTS `{prefix}series_colors` (
    rarity       VARCHAR(10)        NOT NULL,
    series       VARCHAR(10)        NOT NULL,
    PRIMARY KEY (series_id)
-   FOREIGN KEY (series_id) REFERENCES `{prefix}series`(series_id)
 ) DEFAULT CHARSET = utf8mb4;
 
 CREATE TABLE IF NOT EXISTS `{prefix}custom_types` (
     type_id         VARCHAR(200)       NOT NULL,
     display_name    TINYTEXT           NOT NULL,
-    drop_type       ENUM('boss','hostile','neutral','passive','all')
+    drop_type       ENUM('boss','hostile','neutral','passive','all'),
     PRIMARY KEY (type_id)
 ) DEFAULT CHARSET = utf8mb4;
 
 CREATE TABLE IF NOT EXISTS `{prefix}packs` (
-    pack_id         VARCHAR(200)       NOT NULL, -- Foreign key for packs_content
+    pack_id         VARCHAR(200)       NOT NULL,
     display_name    TINYTEXT           NOT NULL,
     buy_price       DOUBLE             NOT NULL,
     permission      VARCHAR(200)       NOT NULL,
@@ -67,28 +63,23 @@ CREATE TABLE IF NOT EXISTS `{prefix}packs_content` (
     `id`             INT                NOT NULL,
     line_number      INT                NOT NULL,
     pack_id          VARCHAR(200)       NOT NULL,
-    rarity_id        VARCHAR(200)       NOT NULL, -- foreign key from rarities
+    rarity_id        VARCHAR(200)       NOT NULL,
     card_amount      VARCHAR(200)       NOT NULL,
-    series_id        VARCHAR(200)       NOT NULL, -- foreign key from series
-    PRIMARY KEY (`id`),
-    FOREIGN KEY (rarity_id) REFERENCES `{prefix}rarities`(rarity_id),
-    FOREIGN KEY (series_id) REFERENCES `{prefix}series`(series_id)
+    series_id        VARCHAR(200)       NOT NULL,
+    PRIMARY KEY (`id`)
 ) DEFAULT CHARSET = utf8mb4;
 
 CREATE TABLE IF NOT EXISTS `{prefix}cards` (
     `id`                INT                 NOT NULL,
-    card_id             VARCHAR(200)        NOT NULL, -- This is not the key, since we can have duplicates.
+    card_id             VARCHAR(200)        NOT NULL,
     display_name        TINYTEXT,
-    rarity_id           VARCHAR(200)        NOT NULL, -- foreign key from rarities
+    rarity_id           VARCHAR(200)        NOT NULL,
     has_shiny           BOOL                NOT NULL,
-    series_id           VARCHAR(200)        NOT NULL, -- foreign key from series
+    series_id           VARCHAR(200)        NOT NULL,
     info                TEXT,
     custom_model_data   INT,
     buy_price           DOUBLE,
     sell_price          DOUBLE,
     type_id             VARCHAR(200)        NOT NULL,
-    PRIMARY KEY (`id`),
-    FOREIGN KEY (rarity_id) REFERENCES `{prefix}rarities`(rarity_id),
-    FOREIGN KEY (series_id) REFERENCES `{prefix}series`(series_id),
-    FOREIGN KEY (type_id) REFERENCES `{prefix}custom_types`(type_id),
+    PRIMARY KEY (`id`)
 ) DEFAULT CHARSET = utf8mb4;
