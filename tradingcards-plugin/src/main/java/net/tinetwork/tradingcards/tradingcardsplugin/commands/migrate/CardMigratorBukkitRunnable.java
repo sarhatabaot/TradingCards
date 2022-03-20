5e6aa74b0c193cc74b218692616478ea5ff50b3a
Omer Oreg <sarhatabaot1@gmail.com>
2022-03-20 10:46:35 +0200

    Impl CardMigration

--- tradingcards-plugin/src/main/java/net/tinetwork/tradingcards/tradingcardsplugin/commands/migrate/CardMigratorBukkitRunnable.java
+++ tradingcards-plugin/src/main/java/net/tinetwork/tradingcards/tradingcardsplugin/commands/migrate/CardMigratorBukkitRunnable.java
@@ -1,13 +1,35 @@
 package net.tinetwork.tradingcards.tradingcardsplugin.commands.migrate;
 
-import org.bukkit.scheduler.BukkitRunnable;
+import net.tinetwork.tradingcards.tradingcardsplugin.TradingCards;
+import net.tinetwork.tradingcards.tradingcardsplugin.card.TradingCard;
+import net.tinetwork.tradingcards.tradingcardsplugin.storage.Storage;
+import net.tinetwork.tradingcards.tradingcardsplugin.storage.impl.local.YamlStorage;
+import org.bukkit.command.CommandSender;
+import org.spongepowered.configurate.ConfigurateException;
 
 /**
  * @author sarhatabaot
  */
-public class CardMigratorBukkitRunnable extends BukkitRunnable {
-    @Override
-    public void run() {
+public class CardMigratorBukkitRunnable extends MigratorBukkitRunnable {
+    public CardMigratorBukkitRunnable(final TradingCards plugin, final CommandSender sender) {
+        super(plugin, sender);
+    }
 
+    @Override
+    public void onExecute() throws ConfigurateException {
+        YamlStorage yamlStorage = new YamlStorage(plugin);
+        yamlStorage.init(plugin);
+        for(TradingCard card: yamlStorage.getCards()) {
+            final String cardId = card.getCardName();
+            final String rarityId = card.getRarity().getName();
+            final String seriesId = card.getSeries().getName();
+            plugin.getStorage().createCard(cardId, rarityId,seriesId);
+            plugin.getStorage().editCardBuyPrice(rarityId,cardId,seriesId,card.getBuyPrice());
+            plugin.getStorage().editCardSellPrice(rarityId,cardId,seriesId,card.getSellPrice());
+            plugin.getStorage().editCardInfo(rarityId,cardId ,seriesId,card.getInfo());
+            plugin.getStorage().editCardCustomModelData(rarityId,cardId,seriesId,card.getCustomModelNbt());
+            plugin.getStorage().editCardType(rarityId,cardId,seriesId,card.getType());
+            plugin.getStorage().editCardDisplayName(rarityId,cardId,seriesId,card.getDisplayName());
+        }
     }
 }
