489f7d994e08a9136aa0406a673a9032e78ed979
Omer Oreg <sarhatabaot1@gmail.com>
2022-03-20 11:21:07 +0200

    Added note about deck migration
    We assume the source type is yaml here. We should change this in the future to support any type of storage.

--- tradingcards-plugin/src/main/java/net/tinetwork/tradingcards/tradingcardsplugin/commands/migrate/DeckMigratorBukkitRunnable.java
+++ tradingcards-plugin/src/main/java/net/tinetwork/tradingcards/tradingcardsplugin/commands/migrate/DeckMigratorBukkitRunnable.java
@@ -2,6 +2,8 @@ package net.tinetwork.tradingcards.tradingcardsplugin.commands.migrate;
 
 import net.tinetwork.tradingcards.api.model.deck.Deck;
 import net.tinetwork.tradingcards.tradingcardsplugin.TradingCards;
+import net.tinetwork.tradingcards.tradingcardsplugin.card.TradingCard;
+import net.tinetwork.tradingcards.tradingcardsplugin.storage.Storage;
 import net.tinetwork.tradingcards.tradingcardsplugin.storage.impl.local.YamlStorage;
 import net.tinetwork.tradingcards.tradingcardsplugin.utils.ChatUtil;
 import net.tinetwork.tradingcards.tradingcardsplugin.utils.Util;
@@ -18,14 +20,14 @@ import java.util.UUID;
  * @author sarhatabaot
  */
 public class DeckMigratorBukkitRunnable extends MigratorBukkitRunnable {
-    public DeckMigratorBukkitRunnable(final TradingCards plugin, final CommandSender sender) {
-        super(plugin, sender);
+    public DeckMigratorBukkitRunnable(final TradingCards plugin, final CommandSender sender, final Storage<TradingCard> source) {
+        super(plugin, sender, source);
     }
 
     @Override
     public void onExecute() throws ConfigurateException {
-        YamlStorage yamlStorage = new YamlStorage(plugin);
-        yamlStorage.init(plugin);
+        //Assume that the source is yaml, change later
+        YamlStorage yamlStorage = (YamlStorage) source;
         Map<UUID, List<Deck>> yamlDecks = yamlStorage.getAllDecks();
         Util.logAndMessage(sender,"Found " + yamlDecks.size() + " players.");
 
