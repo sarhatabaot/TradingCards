77eb5db800c0f59c080a2a8c00cf40659811f623
Omer Oreg <sarhatabaot1@gmail.com>
2022-04-22 13:19:56 +0300

    Added comments to migrations

--- tradingcards-plugin/src/main/resources/db/migration/V1__Create_tables.sql
+++ tradingcards-plugin/src/main/resources/db/migration/V1__Create_tables.sql
@@ -1,6 +1,7 @@
--- TradingCards MySql Schema
-
-CREATE TABLE IF NOT EXISTS `{prefix}decks` (
+-- --------------------------
+-- Table `${prefix}decks`
+-- --------------------------
+CREATE TABLE IF NOT EXISTS `${prefix}decks` (
     id             INT AUTO_INCREMENT NOT NULL,
     uuid           VARCHAR(36)        NOT NULL,
     deck_number    INT                NOT NULL,
@@ -11,7 +12,10 @@ CREATE TABLE IF NOT EXISTS `{prefix}decks` (
     PRIMARY KEY (id)
 ) DEFAULT CHARSET = utf8mb4;
 
-CREATE TABLE IF NOT EXISTS `{prefix}rarities` (
+-- --------------------------
+-- Table `${prefix}rarities`
+-- --------------------------
+CREATE TABLE IF NOT EXISTS `${prefix}rarities` (
     rarity_id      VARCHAR(200)         NOT NULL,
     display_name   TINYTEXT                     ,
     default_color  VARCHAR(36)                  ,
@@ -20,21 +24,30 @@ CREATE TABLE IF NOT EXISTS `{prefix}rarities` (
     PRIMARY KEY (rarity_id)
 ) DEFAULT CHARSET = utf8mb4;
 
-CREATE TABLE IF NOT EXISTS `{prefix}rewards` (
+-- --------------------------
+-- Table `${prefix}rewards`
+-- --------------------------
+CREATE TABLE IF NOT EXISTS `${prefix}rewards` (
     rarity_id      VARCHAR(200)       NOT NULL,
     command        TINYTEXT           NOT NULL,
     command_order  INT                NOT NULL,
     PRIMARY KEY (rarity_id)
 ) DEFAULT CHARSET = utf8mb4;
 
-CREATE TABLE IF NOT EXISTS `{prefix}series` (
+-- --------------------------
+-- Table `${prefix}series`
+-- --------------------------
+CREATE TABLE IF NOT EXISTS `${prefix}series` (
     series_id    VARCHAR(200)                           NOT NULL,
     display_name TINYTEXT                               ,
     series_mode  ENUM('ACTIVE','DISABLED','SCHEDULED')  ,
     PRIMARY KEY (series_id)
 ) DEFAULT CHARSET = utf8mb4;
 
-CREATE TABLE IF NOT EXISTS `{prefix}series_colors` (
+-- -------------------------------
+-- Table `${prefix}series_colors`
+-- -------------------------------
+CREATE TABLE IF NOT EXISTS `${prefix}series_colors` (
    series_id    VARCHAR(200)       NOT NULL,
    `type`       VARCHAR(10)        NOT NULL,
    info         VARCHAR(10)        NOT NULL,
@@ -44,14 +57,20 @@ CREATE TABLE IF NOT EXISTS `{prefix}series_colors` (
    PRIMARY KEY (series_id)
 ) DEFAULT CHARSET = utf8mb4;
 
-CREATE TABLE IF NOT EXISTS `{prefix}custom_types` (
+-- --------------------------
+-- Table `${prefix}custom_types`
+-- --------------------------
+CREATE TABLE IF NOT EXISTS `${prefix}custom_types` (
     type_id         VARCHAR(200)       NOT NULL,
     display_name    TINYTEXT           ,
     drop_type       ENUM('boss','hostile','neutral','passive','all'),
     PRIMARY KEY (type_id)
 ) DEFAULT CHARSET = utf8mb4;
 
-CREATE TABLE IF NOT EXISTS `{prefix}packs` (
+-- --------------------------
+-- Table `${prefix}packs`
+-- --------------------------
+CREATE TABLE IF NOT EXISTS `${prefix}packs` (
     pack_id         VARCHAR(200)       NOT NULL,
     display_name    TINYTEXT           ,
     buy_price       DOUBLE             ,
@@ -59,7 +78,10 @@ CREATE TABLE IF NOT EXISTS `{prefix}packs` (
     PRIMARY KEY (pack_id)
 ) DEFAULT CHARSET = utf8mb4;
 
-CREATE TABLE IF NOT EXISTS `{prefix}packs_content` (
+-- --------------------------
+-- Table `${prefix}packs_content`
+-- --------------------------
+CREATE TABLE IF NOT EXISTS `${prefix}packs_content` (
     `id`             INT                NOT NULL AUTO_INCREMENT,
     line_number      INT                NOT NULL,
     pack_id          VARCHAR(200)       NOT NULL,
@@ -69,7 +91,10 @@ CREATE TABLE IF NOT EXISTS `{prefix}packs_content` (
     PRIMARY KEY (`id`)
 ) DEFAULT CHARSET = utf8mb4;
 
-CREATE TABLE IF NOT EXISTS `{prefix}cards` (
+-- --------------------------
+-- Table `${prefix}cards`
+-- --------------------------
+CREATE TABLE IF NOT EXISTS `${prefix}cards` (
     `id`                INT                 NOT NULL AUTO_INCREMENT,
     card_id             VARCHAR(200)        NOT NULL,
     display_name        TINYTEXT,
--- tradingcards-plugin/src/main/resources/db/migration/V2__Add_series_id.sql
+++ tradingcards-plugin/src/main/resources/db/migration/V2__Add_series_id.sql
@@ -1,1 +1,6 @@
-ALTER TABLE `${prefix}decks` ADD series_id VARCHAR(200) DEFAULT '${default_series_id}';
+-- -------------------------------
+-- Alter Table `${prefix}decks`
+-- -------------------------------
+ALTER TABLE `${prefix}decks`
+    ADD series_id VARCHAR(200)
+    DEFAULT '${default_series_id}';
