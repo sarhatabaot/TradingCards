aa05e3270b92226a7eae8fbec9c577da14cba151
Omer Oreg <sarhatabaot1@gmail.com>
2022-05-13 18:10:18 +0300

    Changed return type from List to Set

--- tradingcards-api/src/main/java/net/tinetwork/tradingcards/api/manager/RarityManager.java
+++ tradingcards-api/src/main/java/net/tinetwork/tradingcards/api/manager/RarityManager.java
@@ -4,13 +4,14 @@ import com.google.common.cache.LoadingCache;
 import net.tinetwork.tradingcards.api.model.Rarity;
 
 import java.util.List;
+import java.util.Set;
 
 /**
  * @author sarhatabaot
  */
 public interface RarityManager {
     Rarity getRarity(final String rarityId);
-    List<Rarity> getRarities();
+    Set<Rarity> getRarities();
     boolean containsRarity(final String rarityId);
 
     /**
