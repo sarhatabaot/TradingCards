e09084eda473781568b03e5f54b6ccbdb1f85911
Omer Oreg <sarhatabaot1@gmail.com>
2022-07-01 23:20:19 +0300

    Adds users table

---
+++ tradingcards-plugin/src/main/resources/db/migration/V5__Add_users_table.sql
@@ -0,0 +1,8 @@
+-- --------------------------
+-- Table `${prefix}users`
+-- --------------------------
+CREATE TABLE IF NOT EXISTS `{prefix}users` (
+    `id`           INT         NOT NULL AUTO_INCREMENT,
+    `uuid`         VARCHAR(36) NOT NULL
+    PRIMARY KEY (id)
+) DEFAULT CHARSET = utf8mb4;
