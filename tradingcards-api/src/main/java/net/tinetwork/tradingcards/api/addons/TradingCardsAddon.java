c015d8cfd4b6e243bf241a1d34ef23cd44029dab
Omer Oreg <sarhatabaot1@gmail.com>
2022-07-03 21:18:27 +0300

    Moved to hocon configurate

--- tradingcards-api/src/main/java/net/tinetwork/tradingcards/api/addons/TradingCardsAddon.java
+++ tradingcards-api/src/main/java/net/tinetwork/tradingcards/api/addons/TradingCardsAddon.java
@@ -1,8 +1,11 @@
 package net.tinetwork.tradingcards.api.addons;
 
 
+import com.github.sarhatabaot.kraken.core.config.HoconConfigurateFile;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.jetbrains.annotations.NotNull;
+
+import java.util.logging.Logger;
 
 /**
  * @author sarhatabaot
@@ -16,14 +19,24 @@ public interface TradingCardsAddon {
 	JavaPlugin getJavaPlugin();
 
 	/**
-	 *
 	 * @return the addon logger.
 	 */
-	AddonLogger getAddonLogger();
+	default Logger getAddonLogger() {
+		return getJavaPlugin().getLogger();
+	}
+
+	/**
+	 * @return the addons' config file
+	 */
+	HoconConfigurateFile<JavaPlugin> getConfig();
+
 
 	/**
 	 * This will execute when we reload the TradingCards main plugin.
+	 * By default, we reload just the config.
 	 */
-	void onReload();
+	default void onReload() {
+		getConfig().reloadConfig();
+	}
 
 }
