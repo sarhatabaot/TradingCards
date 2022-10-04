82f77cc877c171e9ea3c82a77215f46ec72ee1da
Omer Oreg <sarhatabaot1@gmail.com>
2022-10-04 21:02:38 +0300

    Added command impl

--- tradingcards-plugin/src/main/java/net/tinetwork/tradingcards/tradingcardsplugin/commands/BuyCommand.java
+++ tradingcards-plugin/src/main/java/net/tinetwork/tradingcards/tradingcardsplugin/commands/BuyCommand.java
@@ -6,8 +6,11 @@ import co.aikar.commands.annotation.CommandCompletion;
 import co.aikar.commands.annotation.CommandPermission;
 import co.aikar.commands.annotation.Description;
 import co.aikar.commands.annotation.Subcommand;
+import de.tr7zw.nbtapi.NBTCompound;
+import de.tr7zw.nbtapi.NBTItem;
 import net.tinetwork.tradingcards.api.economy.ResponseWrapper;
 import net.tinetwork.tradingcards.api.model.Pack;
+import net.tinetwork.tradingcards.api.utils.NbtUtils;
 import net.tinetwork.tradingcards.tradingcardsplugin.messages.internal.Permissions;
 import net.tinetwork.tradingcards.tradingcardsplugin.TradingCards;
 import net.tinetwork.tradingcards.tradingcardsplugin.card.EmptyCard;
@@ -15,7 +18,12 @@ import net.tinetwork.tradingcards.tradingcardsplugin.card.TradingCard;
 import net.tinetwork.tradingcards.tradingcardsplugin.utils.CardUtil;
 import net.tinetwork.tradingcards.tradingcardsplugin.utils.PlaceholderUtil;
 import org.bukkit.entity.Player;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.PlayerInventory;
 import org.jetbrains.annotations.NotNull;
+
+import java.util.List;
 
 /**
  * @author sarhatabaot
@@ -27,6 +35,7 @@ public class BuyCommand extends BaseCommand {
     public BuyCommand(final TradingCards plugin) {
         this.plugin = plugin;
     }
+
     @Subcommand("buy")
     @CommandPermission(Permissions.BUY)
     public class BuySubCommand extends BaseCommand {
@@ -46,22 +55,101 @@ public class BuyCommand extends BaseCommand {
 
             Pack pack = plugin.getPackManager().getPack(packId);
 
-            if (pack.getBuyPrice() <= 0.0D) {
+            if (pack.getBuyPrice() <= 0.0D && pack.getTradeCards().isEmpty()) {
                 player.sendMessage(plugin.getMessagesConfig().cannotBeBought());
+                return;
+            }
+
+
+            if (!pack.getTradeCards().isEmpty() && !hasCardsInInventory(player, pack.getTradeCards())) {
+                player.sendMessage("Not enough cards to perform a trade in.");
                 return;
             }
 
             ResponseWrapper economyResponse = plugin.getEconomyWrapper().withdraw(player, pack.getCurrencyId(), pack.getBuyPrice());
             if (economyResponse.success()) {
+
                 if (plugin.getGeneralConfig().closedEconomy()) {
-                    plugin.getEconomyWrapper().depositAccount(plugin.getGeneralConfig().serverAccount(),pack.getCurrencyId(),pack.getBuyPrice());
+                    plugin.getEconomyWrapper().depositAccount(plugin.getGeneralConfig().serverAccount(), pack.getCurrencyId(), pack.getBuyPrice());
                 }
+
+                if (!pack.getTradeCards().isEmpty()) {
+                    for (Pack.PackEntry packEntry : pack.getPackEntryList()) {
+                        removeCardsMatchingEntry(player, packEntry);
+                    }
+                    //add bought for cards etc
+                }
+
                 player.sendMessage(plugin.getMessagesConfig().boughtCard().replaceAll(PlaceholderUtil.AMOUNT.asRegex(), String.valueOf(pack.getBuyPrice())));
+
                 CardUtil.dropItem(player, plugin.getPackManager().getPackItem(packId));
                 return;
             }
 
             player.sendMessage(plugin.getMessagesConfig().notEnoughMoney());
+        }
+
+        private void removeCardsMatchingEntry(final @NotNull Player player, final Pack.@NotNull PackEntry packEntry) {
+            PlayerInventory inventory = player.getInventory();
+            /*
+            packentry: common:7:default
+            inventory:
+            zombie:common:2:default, skeleton:common:5:default
+             */
+            int countLeftToRemove = packEntry.amount();
+            for (ItemStack itemStack: inventory.getContents()) {
+                if(matchesEntry(itemStack,packEntry)) {
+                    //accounts for zombie:common:10:default
+                    if(itemStack.getAmount() > countLeftToRemove) {
+                        itemStack.setAmount(itemStack.getAmount() - countLeftToRemove);
+                        plugin.debug(BuyCommand.class,"Removed %d from ItemStack %s, new amount: %s".formatted(countLeftToRemove, itemStack.toString(), itemStack.getAmount()));
+                        break;
+                    }
+
+                    countLeftToRemove -= itemStack.getAmount();
+                    inventory.remove(itemStack);
+                    plugin.debug(BuyCommand.class, "Removed ItemStack %s, amount left to remove %d".formatted(itemStack.toString(),countLeftToRemove));
+                }
+            }
+        }
+
+        private int countCardsInInventory(final @NotNull Player player, final Pack.PackEntry packEntry) {
+            int count = 0;
+            PlayerInventory inventory = player.getInventory();
+
+            for (ItemStack itemStack : inventory.getContents()) {
+                if(matchesEntry(itemStack,packEntry)) {
+                    count += itemStack.getAmount();
+                }
+            }
+            return count;
+        }
+
+        private boolean matchesEntry(final ItemStack itemStack, final Pack.PackEntry packEntry) {
+            NBTItem nbtItem = new NBTItem(itemStack);
+            if (!CardUtil.isCard(nbtItem)) {
+                return false;
+            }
+
+
+            NBTCompound tcCompound = nbtItem.getCompound(NbtUtils.TC_COMPOUND);
+
+            //don't count if it's shiny.
+            if (tcCompound.getBoolean(NbtUtils.TC_CARD_SHINY))
+                return false;
+
+            final String nbtRarity = tcCompound.getString(NbtUtils.TC_CARD_RARITY);
+            final String nbtSeries = tcCompound.getString(NbtUtils.TC_CARD_SERIES);
+
+            return packEntry.seriesId().equals(nbtSeries) && packEntry.getRarityId().equals(nbtRarity);
+        }
+
+        private boolean hasCardsInInventory(final Player player, final @NotNull List<Pack.PackEntry> tradeCards) {
+            for (Pack.PackEntry packEntry : tradeCards) {
+                if (packEntry.amount() > countCardsInInventory(player, packEntry))
+                    return false;
+            }
+            return true;
         }
 
 
@@ -82,10 +170,10 @@ public class BuyCommand extends BaseCommand {
             final double buyPrice = tradingCard.getBuyPrice();
             final String currencyId = tradingCard.getCurrencyId();
 
-            ResponseWrapper economyResponse = plugin.getEconomyWrapper().withdraw(player, currencyId,buyPrice);
+            ResponseWrapper economyResponse = plugin.getEconomyWrapper().withdraw(player, currencyId, buyPrice);
             if (economyResponse.success()) {
                 if (plugin.getGeneralConfig().closedEconomy()) {
-                    plugin.getEconomyWrapper().depositAccount(plugin.getGeneralConfig().serverAccount(), currencyId,buyPrice);
+                    plugin.getEconomyWrapper().depositAccount(plugin.getGeneralConfig().serverAccount(), currencyId, buyPrice);
                 }
                 CardUtil.dropItem(player, tradingCard.build(false));
                 player.sendMessage(plugin.getMessagesConfig().boughtCard().replaceAll(PlaceholderUtil.AMOUNT.asRegex(), String.valueOf(buyPrice)));
