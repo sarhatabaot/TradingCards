a566b63039b58fe1c478c845a9fe057e4b855899
Omer Oreg <sarhatabaot1@gmail.com>
2022-10-05 11:28:37 +0300

    Added missing sql schemas

--- tradingcards-plugin/src/main/resources/db/base/V0_mysql.sql
+++ tradingcards-plugin/src/main/resources/db/base/V0_mysql.sql
@@ -8,6 +8,7 @@ CREATE TABLE IF NOT EXISTS `{prefix}decks` (
     rarity_id      VARCHAR(200)       NOT NULL,
     amount         INT                NOT NULL,
     is_shiny       BOOL               NOT NULL,
+    series_id      VARCHAR(200)       NOT NULL DEFAULT '${default_series_id}',
     PRIMARY KEY (id)
 ) DEFAULT CHARSET = utf8mb4;
 
@@ -17,6 +18,8 @@ CREATE TABLE IF NOT EXISTS `{prefix}rarities` (
     default_color  VARCHAR(36)                  ,
     buy_price      DOUBLE                       ,
     sell_price     DOUBLE                       ,
+    currency_id    VARCHAR(30)          DEFAULT NULL,
+    custom_order   INT                  DEFAULT 0,
     PRIMARY KEY (rarity_id)
 ) DEFAULT CHARSET = utf8mb4;
 
@@ -56,6 +59,7 @@ CREATE TABLE IF NOT EXISTS `{prefix}packs` (
     display_name    TINYTEXT           ,
     buy_price       DOUBLE             ,
     permission      VARCHAR(200)       ,
+    currency_id     VARCHAR(30)        DEFAULT 'tc-internal-default',
     PRIMARY KEY (pack_id)
 ) DEFAULT CHARSET = utf8mb4;
 
@@ -69,17 +73,28 @@ CREATE TABLE IF NOT EXISTS `{prefix}packs_content` (
     PRIMARY KEY (`id`)
 ) DEFAULT CHARSET = utf8mb4;
 
+CREATE TABLE IF NOT EXISTS `{prefix}packs_trade` (
+    `id`            INT                 NOT NULL AUTO_INCREMENT,
+    line_number      INT                NOT NULL,
+    pack_id          VARCHAR(200)       NOT NULL,
+    rarity_id        VARCHAR(200)       NOT NULL,
+    card_amount      VARCHAR(200)       NOT NULL,
+    series_id        VARCHAR(200)       NOT NULL,
+    PRIMARY KEY (`id`)
+) DEFAULT CHARSET = utf8mb4;
+
 CREATE TABLE IF NOT EXISTS `{prefix}cards` (
     `id`                INT                 NOT NULL AUTO_INCREMENT,
     card_id             VARCHAR(200)        NOT NULL,
     display_name        TINYTEXT,
     rarity_id           VARCHAR(200)        NOT NULL,
-    has_shiny           BOOL                        ,
+    has_shiny           BOOL                ,
     series_id           VARCHAR(200)        NOT NULL,
     info                TEXT,
     custom_model_data   INT,
     buy_price           DOUBLE,
     sell_price          DOUBLE,
     type_id             VARCHAR(200),
+    currency_id         VARCHAR(30)        DEFAULT 'tc-internal-default',
     PRIMARY KEY (`id`)
 ) DEFAULT CHARSET = utf8mb4;
