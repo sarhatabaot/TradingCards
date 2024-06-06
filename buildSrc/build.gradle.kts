588bff60ee215767d0ea71e6d2547c1cf3cb84cc
Omer Oreg <omeroreg@gmail.com>
2024-06-06 13:19:34 +0300

    Update kotlin gradle plugin

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 }
 
 dependencies {
-    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.23")
+    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:2.0.0")
 }
 repositories {
     // Use the plugin portal to apply community plugins in convention plugins.
